d79a4a7f7bf88f2f5c3f9957f55fc526c57f97bc
Merroks <89959496+Merroks@users.noreply.github.com>
2022-09-30 18:32:14 +1000

    Homework 5 start

--- src/main/java/com/geekbrains/builder/Main.java
+++ src/main/java/com/geekbrains/builder/Main.java
@@ -1,30 +1,32 @@
-//package com.geekbrains.builder;
-//
-//import com.geekbrains.api.ApiSearchResult;
-//import com.geekbrains.api.ApiUserConnectRequest;
-//import com.geekbrains.api.ApiUserConnectResult;
-//import com.geekbrains.api.SpoonaccularService;
-//
-//import org.apache.ibatis.io.Resources;
-//import org.apache.ibatis.session.SqlSession;
-//import org.apache.ibatis.session.SqlSessionFactory;
-//import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-//import com.geekbrains.db.dao.CategoriesMapper;
-//import com.geekbrains.db.dao.ProductsMapper;
-//import com.geekbrains.db.model.Categories;
-//import com.geekbrains.db.model.Products;
-//import java.io.IOException;
-//
-//public class Main {
-//    public static void main(String[] args) throws IOException {
-//
-////        SpoonaccularService spoonaccularService = new SpoonaccularService();
-////        ApiSearchResult recipes = spoonaccularService.findRecipes("Bread", 3);
-////        System.out.println(recipes);
-////
-////        ApiUserConnectResult connectResult = spoonaccularService.connect(new ApiUserConnectRequest());
-////        System.out.println(connectResult);
-//
+package com.geekbrains.builder;
+
+import com.geekbrains.api.ApiSearchResult;
+import com.geekbrains.api.ApiUserConnectRequest;
+import com.geekbrains.api.ApiUserConnectResult;
+import com.geekbrains.api.SpoonaccularService;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import com.geekbrains.db.dao.CategoriesMapper;
+import com.geekbrains.db.dao.ProductsMapper;
+import com.geekbrains.db.model.Categories;
+import com.geekbrains.db.model.Products;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        //К 5-му ДЗ
+        SpoonaccularService spoonaccularService = new SpoonaccularService();
+        ApiSearchResult recipes = spoonaccularService.findRecipes("Bread", 3);
+        System.out.println(recipes);
+
+        ApiUserConnectResult connectResult = spoonaccularService.connect(new ApiUserConnectRequest());
+        System.out.println(connectResult);
+
+        //К 6-му ДЗ
 //        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder()
 //                .build(Resources.getResourceAsStream("myBatisConfig.xml"));
 //
@@ -37,6 +39,7 @@
 //            System.out.println(category);
 //
 //        }
-//
-//    }
-//}
+
+
+    }
+}
